73098f415b411bc03e49965347b76e8f20b7ca90
Gilberto Oliveira <gilbertoino@rvtecnologia.com.br>
2019-04-11 15:31:27 -0300

    opna \n quebrou 1 /n qebrou 2

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# botExamplo
+# botExemplo
+
+Conforme exemplo https://pythonspot.com/category/selenium/
+Teste realizado usando python 3.7 no Windows 10
+
+###  LInk para exemplos
+https://github.com/pedebodes/SeleniumBase
+
+# para executar 
+pytest  bot_senelium.py  --browser=chrome
---
+++ bot_senelium.py
@@ -0,0 +1,40 @@
+from seleniumbase import BaseCase
+import time
+class MyTestClass(BaseCase):
+
+    # def test_basic(self):
+    #     self.open("https://xkcd.com/353/")
+    #     self.assert_element('img[alt="Python"]')
+    #     self.click('a[rel="license"]')
+    #     self.assert_text("free to copy", "div center")
+    #     self.open("https://xkcd.com/1481/")
+    #     title = self.get_attribute("#comic img", "title")
+    #     self.assert_true("86,400 seconds per day" in title)
+    #     self.click("link=Blag")
+    #     self.assert_text("The blag of the webcomic", "h2")
+    #     self.update_text("input#s", "Robots!\n")
+    #     self.assert_text("Hooray robots!", "#content")
+    #     self.open("https://xkcd.com/1319/")
+    #     self.assert_exact_text("Automation", "#ctitle")   
+
+    def test_basic(self):
+        
+        self.open("https://xkcd.com/1481/")
+        time.sleep(15)
+        self.click("link=Blag")
+        self.assert_text("The blag of the webcomic", "h2")
+        self.update_text("input#s", "Robots!\n")
+        self.assert_text("Hooray robots!", "#content")
+        self.open("https://xkcd.com/1319/")
+        self.assert_exact_text("Automation", "#ctitle")
+
+    # def test_delayed_asserts(self):
+    #     self.open('https://xkcd.com/993/')
+    #     self.wait_for_element('#comic')
+    #     self.delayed_assert_element('img[alt="Brand Identity"]')
+    #     self.delayed_assert_element('img[alt="Rocket Ship"]')  # Will Fail
+    #     self.delayed_assert_element('#comicmap')
+    #     self.delayed_assert_text('Fake Item', '#middleContainer')  # Will Fail
+    #     self.delayed_assert_text('Random', '#middleContainer')
+    #     self.delayed_assert_element('a[name="Super Fake !!!"]')  # Will Fail
+    #     self.process_delayed_asserts()
